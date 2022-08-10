4111d2d8459a81872e4d9e62382aa685455192f5
SUNGHO-DESKTOP\andyk <andykr1234@gmail.com>
2022-08-10 11:18:19 +0900

    상품 엔티티개발(비즈니스로직추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughtStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,4 +28,25 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==//
+
+    /**
+     * 재고증가로직
+     * @param stockQuantity
+     */
+    public void addStock(int stockQuantity){
+        this.stockQuantity += stockQuantity;
+    }
+
+    /**
+     * 재고감소로직
+     * @param stockQuantity
+     */
+    public void removeStock(int stockQuantity){
+        int restStock = this.stockQuantity - stockQuantity;
+        if(restStock<0){
+            throw new NotEnoughtStockException("재고가 부족합니다");
+        }
+        this.stockQuantity = restStock;
+    }
 }
